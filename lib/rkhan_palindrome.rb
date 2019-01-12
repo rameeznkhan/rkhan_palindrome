7631035c7a156488f5bde8346aa426abaea8ef0e
rameez <rameeznkhan@gmail.com>
2019-01-12 16:48:20 +0530

    Factor into module, add integer palindrome detection

--- lib/rkhan_palindrome.rb
+++ lib/rkhan_palindrome.rb
@@ -1,12 +1,20 @@
 require "rkhan_palindrome/version"
 
-class String
+module RKhanModulePalindrome
   def palindrome?
     processed_content == processed_content.reverse
   end
 
   private
   def processed_content
-    self.scan(/[a-z]/i).join.downcase
+    self.to_s.scan(/[a-z\d]/i).join.downcase
   end
+end
+
+class String
+  include RKhanModulePalindrome
+end
+
+class Integer
+  include RKhanModulePalindrome
 end
